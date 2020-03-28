f83e6db39271400eb9a8cebc62f125b5a30377e5
Alerson <alerson.rigo@gmail.com>
2020-03-28 17:50:03 -0300

    Creating model class from Categorie

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,10 +1,12 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 
+import { Categorie } from "./pages/categories/shared/categorie.model";
+
 // ESSA CLASSE IRA FAZER O PAPEL DE SIMULAR UM BANDO DE DADOS RETORNANDO
 // O OBJETOS CRIADO DENTRO DA CONSTANTE CATEGORIES
 export class InMemoryDatabase implements InMemoryDbService {
     createDb(){
-        const categories = [
+        const categories: Categorie[] = [
             { id: 1, name:  "Moradia", description: 'Pagamentos de Contas da Casa' },
             { id: 2, name: "Saúde", description: 'Plano de Saúde e Remédios' },
             { id: 3, name: 'Lazer', description: 'Ciname, Parque, Praia, etc' },
